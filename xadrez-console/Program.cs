8bec4facc6f1d3543c68903fd1fb5a6e43bde97c
Vitor Almeida <gabrielronaldo9343@gmail.com>
2025-06-20 10:22:53 -0400

    Adicionando validações e exceções

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using tabuleiro;
+using xadrez_console;
+using xadrez;
 
 namespace XadrezConole
 {
@@ -8,6 +10,12 @@ namespace XadrezConole
         {
 
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+            Tela.ImprimirTabuleiro(tab);
 
             Console.ReadLine();
         }
